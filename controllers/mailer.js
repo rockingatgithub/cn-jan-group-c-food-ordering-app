4a46eb27ef60e3ccf539ad6705cd55b0f8aa95ea
singhzrepo <contactwithsid@gmail.com>
2022-10-25 19:14:28 +0530

    removed creds

--- controllers/mailer.js
+++ controllers/mailer.js
@@ -10,8 +10,8 @@ exports.mailer = async  () => {
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
-      user: 'cyberkingsid@gmail.com', // generated ethereal user
-      pass: 'lwsqncckpfvujrlk',
+      user: '', // generated ethereal user
+      pass: '',
     },
   });
 
