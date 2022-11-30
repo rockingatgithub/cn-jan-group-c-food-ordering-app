9b72162fe2f898434a3395ea9010ac820cbcdb93
singhzrepo <contactwithsid@gmail.com>
2022-11-30 19:54:05 +0530

    added remote db address

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/food_ordering_app')
+mongoose.connect('mongodb+srv://sudhendra:z6Invz9TQWtNxs0U@cluster0.uy2kh.mongodb.net/?retryWrites=true&w=majority')
 
 const db = mongoose.connection
 
